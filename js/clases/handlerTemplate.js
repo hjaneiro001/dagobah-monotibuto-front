5af4571bef942b1ec0650656fbe32fc2e4514c4a
hjaneiro001 <hjaneiro@lefkas.com.ar>
2025-01-25 19:27:43 -0300

    inicio comimit en rama

---
+++ js/clases/handlerTemplate.js
@@ -0,0 +1,26 @@
+export class handlerTemplate {
+
+    async html(url) {
+        let response = await fetch(url)
+        return await response.text()
+    }
+
+    // Pages
+    async getHome(){
+        return await this.html("../pages/home.html")
+    }
+
+    async getFacturacion(){
+        return await this.html("../pages/facturacion.html")
+    }
+  
+    async getClientes(){
+        return await this.html("./pages/clientes.html")
+    }
+
+    async getProductos(){
+        return await this.html("./pages//productos.html")
+    }
+
+
+}
---
+++ js/sources/app-common.js
@@ -0,0 +1,104 @@
+import {
+  handlerTemplate
+} from "../clases/handlerTemplate.js"
+
+/////////////////////////////////////
+//                                 //
+//         MANEJO SPA              //
+//                                 //
+/////////////////////////////////////
+
+const ctnHome = document.getElementById("home-section")
+const ctnFacturacion = document.getElementById("facturacion-section")
+const ctnClientes = document.getElementById("clientes-section")
+const ctnProductos = document.getElementById("productos-section")
+
+const plantilla = new handlerTemplate
+
+async function mostrarOcultar() {
+
+  let response
+
+  ctnHome.classList.add('d-none')
+  ctnFacturacion.classList.add("d-none")
+  ctnClientes.classList.add("d-none")
+  ctnProductos.classList.add("d-none")
+
+  switch (location.hash) {
+
+    case "#/home":
+
+      ctnHome.classList.remove("d-none")
+      if (ctnHome.getAttribute("inicio") == "false") {
+        $("#home-section").html(await plantilla.getHome())
+        ctnHome.setAttribute("inicio", "true")
+      }
+      break;
+
+    case "#/facturacion":
+
+      ctnFacturacion.classList.remove("d-none")
+      if (ctnFacturacion.getAttribute("inicio") == "false") {
+        $("#facturacion-section").html(await plantilla.getFacturacion())
+        ctnMateriasPrimas.setAttribute("inicio", "true")
+      }
+      break;
+
+    case "#/clientes":
+
+      ctnClientes.classList.remove("d-none")
+      if (ctnClientes.getAttribute("inicio") == "false") {
+        $("#clientes-section").html(await plantilla.getClientes())
+        ctnClientes.setAttribute("inicio", "true")
+      }
+      break;
+
+    case "#/productos":
+
+    ctnProductos.classList.remove("d-none")
+    if (ctnProductos.getAttribute("inicio") == "false") {
+      $("#productos-section").html(await plantilla.getProductos())
+      ctnProductos.setAttribute("inicio", "true")
+    }
+    break;
+
+
+  }
+
+
+}
+
+window.addEventListener("hashchange", mostrarOcultar)
+
+// Delega eventos a varios elementos (Upgradear a Delegacion de eventos es mas nuevo)
+var botonHome = document.querySelectorAll(".boton-menu-home")
+botonHome.forEach(element => {
+  element.addEventListener("click", () => {
+    location.hash = "/home"
+  });
+})
+
+document.getElementById("boton-menu-facturacion").addEventListener("click", () => {
+  location.hash = "/facturacion"
+})
+
+document.getElementById("boton-menu-clientes").addEventListener("click", () => {
+  location.hash = "/clientes"
+})
+
+document.getElementById("boton-menu-productos").addEventListener("click", () => {
+  location.hash = "/productos"
+})
+
+
+if (!location.hash) {
+  location.hash = "/home"
+} else {
+  mostrarOcultar()
+}
+
+/////////////////////////////////////////////
+//                                         //
+//           FIN MANEJO SPA                //
+//                                         //
+/////////////////////////////////////////////
