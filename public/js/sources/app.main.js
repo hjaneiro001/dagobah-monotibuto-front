32f104f4f830f5cc4e0710e351d54c53adfc186c
hjaneiro001 <hjaneiro@lefkas.com.ar>
2025-05-18 03:13:53 -0300

    borra cookie luego de grabar en sessionstorage

--- public/js/sources/app.main.js
+++ public/js/sources/app.main.js
@@ -12,6 +12,8 @@ if (token) {
   sessionStorage.setItem("token", token);
 }
 
+document.cookie = "token=; path=/; domain=.tatooine.com.ar; expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; SameSite=None";
+
 // Si no hay token en sessionStorage, redirigir al login
 if (!sessionStorage.getItem("token")) {
   window.location.href = "https://stage.login.tatooine.com.ar?url=stage.dagobah.tatooine.com.ar";
